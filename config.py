7f29c6e3f9df9084c2f51c0b4f1407d006492651
nickchristo <nick.christophides@gmail.com>
2026-07-17 15:24:51 -0400

    Update basket of equities

--- config.py
+++ config.py
@@ -4,19 +4,16 @@ tickers = ['AGIX',    # KraneShares ETF     Sector: AI ETF
            'CAT',     # Caterpillar         Sector: Industrials
            'COST',    # Costco              Sector: Consumer Staples
            'GE',      # GE Aerospace        Sector: Industrials
-           'GEV',     # GE Vernova          Sector: Industrials
-           'HD',      # Home Depot          Sector: Consumer Disc.
-           'IEX',     # IDEX Corp.          Sector: Industrials
+           'GLD',     # SPDR Gold ETF       Sector: Commodities
            'JNJ',     # Johnson & Johnson   Sector: Health Care
            'MSFT',    # Microsoft           Sector: Information Tech
            'MU',      # Micron              Sector: Information Tech
            'NEE',     # NextEra Energy      Sector: Utilities
            'NVDA',    # NVIDIA              Sector: Information Tech
-           'PG',      # Proctor & Gamble    Sector: Consumer Staples
            'PLD',     # Prologis            Sector: Real Estate
-           'SPY',     # S&P 500             Market Index
-           'TSLA',    # Tesla               Sector: Consumer Disc.
+           'SPY',     # S&P 500             Sector: Market Index
            'UNH',     # UnitedHealth        Sector: Health Care
            'V',       # Visa                Sector: Financials
            'VTRS',    # Viatris             Sector: Healthcare
+           'XLP',     # Sector ETF          Sector: Consumer Staples
            'XOM']     # ExxonMobil          Sector: Energy
